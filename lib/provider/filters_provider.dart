7be575698275087011b7cc0978dff0abe7d88aed
Sampurn Chouksey <sampurn10chouksey@gmail.com>
2023-07-17 23:10:31 +0530

    'App completed using riverpod'

--- lib/provider/filters_provider.dart
+++ lib/provider/filters_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/provider/meal_provider.dart';
 
 enum Filter {
   glutonfree,
@@ -15,6 +16,10 @@ class FilterNotifier extends StateNotifier<Map<Filter, bool>> {
           Filter.vegetarian: false,
           Filter.vegan: false,
         });
+  void setFilters(Map<Filter, bool> chosenfilter) {
+    state = chosenfilter;
+  }
+
   void Setfilter(Filter filter, bool isActive) {
     state = {...state, filter: isActive};
   }
@@ -24,3 +29,23 @@ final filtersProvider =
     StateNotifierProvider<FilterNotifier, Map<Filter, bool>>(
   (ref) => FilterNotifier(),
 );
+
+final FilteredMealProvider = Provider((ref) {
+  final meals = ref.watch(mealProvider);
+  final activefilter = ref.watch(filtersProvider);
+  return meals.where((meal) {
+    if (activefilter[Filter.glutonfree]! && !meal.isGlutenFree) {
+      return false;
+    }
+    if (activefilter[Filter.lactosefree]! && !meal.isLactoseFree) {
+      return false;
+    }
+    if (activefilter[Filter.vegan]! && !meal.isVegan) {
+      return false;
+    }
+    if (activefilter[Filter.vegetarian]! && !meal.isVegetarian) {
+      return false;
+    }
+    return true;
+  }).toList();
+});
