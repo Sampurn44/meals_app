4cf8e935de783709401a5deb3c6d96aef0a2704a
Sampurn Chouksey <sampurn10chouksey@gmail.com>
2023-07-18 01:15:12 +0530

    "Explicit Animation Done!"

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -75,9 +75,13 @@ class _CategoriesScreenState extends State<CategoriesScreen>
           context,
           child,
         ) =>
-            Padding(
-              padding: EdgeInsets.only(
-                top: 100 - _animationController.value * 100,
+            SlideTransition(
+              position: Tween(
+                begin: const Offset(0, 0.3),
+                end: const Offset(0, 0),
+              ).animate(
+                CurvedAnimation(
+                    parent: _animationController, curve: Curves.easeInOut),
               ),
               child: child,
             ));
